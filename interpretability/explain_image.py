a1ac161fe13668fde2250ec3fab3d25c425699cd
13felixblakemore <13felix.blakemore@gmail.com>
2026-01-15 16:08:29 +0000

    explain image script

---
+++ interpretability/explain_image.py
@@ -0,0 +1,79 @@
+import argparse
+import bcos.common.BcosUtilMixin
+
+from bcos import BcosUtilMixin
+
+
+def get_parser(add_help=True):
+    parser = argparse.ArgumentParser(
+        description="Explain an image", add_help=add_help
+    )
+    parser.add_argument(
+        "--base_directory",
+        default="./experiments",
+        help="The base directory.",
+    )
+    parser.add_argument(
+        "--dataset",
+        choices=["ImageNet", "CIFAR10", "ImageNette"],
+        default="ImageNette",
+        help="The dataset.",
+    )
+    parser.add_argument(
+        "--base_network", help="The model config or base network to use."
+    )
+    parser.add_argument("--experiment_name", help="The name of the experiment to run.")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "--reload", help="What ckpt to load. ['last', 'best', 'epoch_<N>', 'best_any']"
+    )
+    group.add_argument(
+        "--weights",
+        metavar="PATH",
+        type=Path,
+        help="Specific weight state dict to load.",
+    )
+
+    parser.add_argument(
+        "--ema",
+        default=False,
+        action="store_true",
+        help="Load the EMA stored version if it exists. Not applicable for reload='best_any'.",
+    )
+
+    parser.add_argument(
+        "--batch_size", type=int, default=1, help="Batch size to use. Default is 1"
+    )
+    parser.add_argument(
+        "--no-cuda",
+        default=False,
+        action="store_true",
+        help="Force into not using cuda.",
+    )
+
+    parser.add_argument(
+        "--image_path", type=str, help="The image path."
+    )
+
+    return parser
+
+def explain_image(args, image_path):
+    model, config = load_model_and_config(args)
+    model.eval()
+
+    expl_out = model.explain(img)
+    print("Prediction:", idx2label[expl_out["prediction"]])
+
+    plt.imshow(expl_out["explanation"])
+    path_to_save = str(args.base_directory / f"explanation.png")
+
+    # Saving the plot
+    plt.savefig(path_to_save, bbox_inches='tight')
+    plt.close()
+
+if __name__ == "main":
+    parser = get_parser()
+    args = parser.parse_args()
+    image = args.image_path
+    explain_image(args, image)
