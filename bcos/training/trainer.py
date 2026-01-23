bf889ec7f9d5f613c86ca53aa5acf6b2f736b2ea
13felixblakemore <13felix.blakemore@gmail.com>
2026-01-23 23:31:51 +0000

    eval step dict

--- bcos/training/trainer.py
+++ bcos/training/trainer.py
@@ -785,8 +785,8 @@ class ClassificationLitModel(pl.LightningModule):
         return loss
 
     def eval_step(self, batch, _batch_idx, val_or_test):
-        
-        images, labels = batch
+        images = batch["video"]
+        labels = batch["label"]
         clip_kd = self.config.get("clip_kd", False)
         if clip_kd:
             output_clip, output_bcos = self(images)
