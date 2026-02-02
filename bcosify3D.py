37cbc3c11493fe654a3a0be413892a02b043ac95
13felixblakemore <13felix.blakemore@gmail.com>
2026-02-02 12:57:05 +0000

    d

--- bcosify3D.py
+++ bcosify3D.py
@@ -61,6 +61,7 @@ class BcosifyNetwork(BcosUtilMixin, nn.Module):
         return (x.float() / 255.0 - mean) / std
 
     def forward(self, x):
+        x = x.float()
         out = self.bcosifynormalize(x)  # [B, 101, 1, 1, 1]
         print("bcosifynorm output: ", out.shape)
         out = self.model(out)
