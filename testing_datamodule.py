3673869eb26451b429291a5023871ab7bc941a5c
13felixblakemore <13felix.blakemore@gmail.com>
2026-02-03 15:39:54 +0000

    testing dm

---
+++ testing_datamodule.py
@@ -0,0 +1,22 @@
+from bcos.data.datamodules import UCF101DataModule
+from bcos.experiments.utils import Experiment
+
+try:
+    from tqdm.auto import tqdm
+except ImportError:
+    tqdm = lambda x: x
+
+exp = Experiment("UCF101", "bcosification", "i3d")
+
+config = exp.config  # gets the config
+
+datamodule = UCF101DataModule(config)
+i=0
+
+
+for video, target in tqdm(datamodule):
+    print(target.numpy())
+    i = i + 1
+    if i == 20:
+        break
+
