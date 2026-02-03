da447ef810adc05d8b5697acadb3fd8e124352b9
13felixblakemore <13felix.blakemore@gmail.com>
2026-02-03 11:23:43 +0000

    print

--- bcos/training/trainer.py
+++ bcos/training/trainer.py
@@ -519,10 +519,10 @@ class ClassificationLitModel(pl.LightningModule):
                 task="multiclass", top_k=5, num_classes=num_classes, compute_on_cpu=True
             )
             self.eval_acc1 = torchmetrics.Accuracy(
-                task="binary", top_k=1, num_classes=num_classes, compute_on_cpu=True
+                task="multiclass", top_k=1, num_classes=num_classes, compute_on_cpu=True
             )
             self.eval_acc5 = torchmetrics.Accuracy(
-                task="binary", top_k=5, num_classes=num_classes, compute_on_cpu=True
+                task="multiclass", top_k=5, num_classes=num_classes, compute_on_cpu=True
             )
 
             has_ema = self.config.get("ema", None) is not None
