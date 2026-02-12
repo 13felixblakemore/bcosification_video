e1094b53ee0c0ed0b023d781b2faa1f25e171de4
13felixblakemore <13felix.blakemore@gmail.com>
2026-02-12 12:38:44 +0000

    d

--- bcos/training/training.py
+++ bcos/training/training.py
@@ -78,7 +78,7 @@ def run_training(args):
         logger=loggers,
         callbacks=callbacks,
         accumulate_grad_batches=8,
-        precision="bf16",
+        precision=16,
         **trainer_config,
     )
     print("GPUS: ", trainer.num_devices)
