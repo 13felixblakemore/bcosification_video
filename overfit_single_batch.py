7899cfdab566299ffc45357dd6ebca47e6b9e753
13felixblakemore <13felix.blakemore@gmail.com>
2026-02-19 21:16:19 +0000

    testing

--- overfit_single_batch.py
+++ overfit_single_batch.py
@@ -1,10 +1,12 @@
 import torch
 from torch.utils.data import DataLoader
+from torch.utils.data.datapipes.dataframe.dataframe_wrapper import iterate
 from torchvision.datasets import UCF101
 
 from bcos import settings
 from bcos.data.datamodules import VideoOnlyDataset
 from bcos.data.presets import UCF101ClassificationPresetEval, UCF101ClassificationPresetTrain
+from bcos.models.standard_models import I3DBcos
 
 train_md = torch.load("ucf101_train_metadata.pt")
 
@@ -34,13 +36,32 @@ train_dataset = VideoOnlyDataset(train_dataset)
 train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=16)
 
 # ====== INSPECT VIDEOS & LABELS ======
-for i, (video, label) in enumerate(train_loader):
-    # video: [B, T, C, H, W]
-    # audio: optional, ignore if not needed
-    # label: tensor of size [B]
+"""for i, (video, label) in enumerate(train_loader):
+    # video: [B, C, T, H, W]
     print("Batch", i)
     print("Labels:", label)
     print("Video:", video.shape)
 
     if i >= 10:  # just first 10 batches
-        break
+        break"""
+
+videos, labels = next(iter(train_loader))
+videos, labels = videos.cuda(), labels.cuda()
+
+model = I3DBcos
+epochs = 100
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+model.train()
+model.cuda()
+
+
+for i in range(epochs):
+    optimizer.zero_grad()
+    outputs = model(videos)
+    loss = criterion(outputs, labels)
+    loss.backward()
+    optimizer.step()
+    if i % 10 == 0:
+        print(f"Step {i}: loss = {loss.item()}")
