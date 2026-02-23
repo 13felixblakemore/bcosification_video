1ad872b710dc30b4bce7b91a4829a42f55acb377
13felixblakemore <13felix.blakemore@gmail.com>
2026-02-23 12:34:36 +0000

    freeze bb

--- bcosify3D.py
+++ bcosify3D.py
@@ -115,6 +115,13 @@ class BcosifyNetwork(BcosUtilMixin, nn.Module):
                     # compound module, go inside it
                     cls.bcosify(module, model_config)
 
+            if not n.startswith("blocks.6"):
+                for p in module.parameters():
+                    p.requires_grad = False
+            else:
+                for p in module.parameters():
+                    p.requires_grad = True
+
             norm_layer = model_config['bcosify_args'].get('norm_layer', 'BnUncV2')
             # What is global average pooling?
             gap = model_config['bcosify_args'].get('gap',
