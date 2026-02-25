13098756bc39c4b25fa5cfc1de62ee3b2c73b703
13felixblakemore <13felix.blakemore@gmail.com>
2026-02-25 12:59:53 +0000

    devices3

--- bcos/training/training.py
+++ bcos/training/training.py
@@ -84,7 +84,7 @@ def run_training(args):
     trainer = pl.Trainer(
         default_root_dir=save_dir,
         accelerator="auto",
-        devices=3,
+        devices=2,
         logger=loggers,
         callbacks=callbacks,
         **trainer_config,
