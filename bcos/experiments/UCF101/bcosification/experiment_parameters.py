060163106d2ecdb49d1af21c5534e3a75b0b64cc
13felixblakemore <13felix.blakemore@gmail.com>
2026-02-28 17:02:44 +0000

    b size and clip lentgh

--- bcos/experiments/UCF101/bcosification/experiment_parameters.py
+++ bcos/experiments/UCF101/bcosification/experiment_parameters.py
@@ -26,7 +26,7 @@ NUM_CLASSES = 101
 NUM_TRAIN_EXAMPLES: int = 50000 # not sure
 NUM_EVAL_EXAMPLES: int = 3925 # not sure
 
-DEFAULT_BATCH_SIZE = 16  # could be causing noisy gradients?
+DEFAULT_BATCH_SIZE = 32  # could be causing noisy gradients?
 DEFAULT_NUM_EPOCHS = 20
 DEFAULT_LR =1e-5
 DEFAULT_CROP_SIZE = 224
