a8a6137078da04374a6d90f7b18154f9f4f86afe
13felixblakemore <13felix.blakemore@gmail.com>
2026-03-01 14:07:21 +0000

    lr

--- bcos/experiments/UCF101/bcosification/experiment_parameters.py
+++ bcos/experiments/UCF101/bcosification/experiment_parameters.py
@@ -28,7 +28,7 @@ NUM_EVAL_EXAMPLES: int = 3925 # not sure
 
 DEFAULT_BATCH_SIZE = 8  # could be causing noisy gradients?
 DEFAULT_NUM_EPOCHS = 20
-DEFAULT_LR =1e-4
+DEFAULT_LR =1e-3
 DEFAULT_CROP_SIZE = 224
 
 DEFAULT_NORM_LAYER = norms.NoBias(norms.BatchNormUncentered3d)  # bnu-linear
