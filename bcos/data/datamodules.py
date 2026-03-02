1dd9f24c3308fece294afafc95d24bae59c4ab8a
13felixblakemore <13felix.blakemore@gmail.com>
2026-03-02 12:05:09 +0000

    clip frames

--- bcos/data/datamodules.py
+++ bcos/data/datamodules.py
@@ -342,10 +342,10 @@ class UCF101DataModule(ClassificationDataModule):
             self.train_dataset = UCF101(
                 root=settings.UCF101_PATH,
                 annotation_path=self._TRAIN_PATH,
-                frames_per_clip=28,
+                frames_per_clip=24,
                 fold=2,
                 transform=self.config["train_transform"],
-                step_between_clips=64,
+                step_between_clips=72,
                 train=True,
                 _precomputed_metadata=train_md,
             )
@@ -363,10 +363,10 @@ class UCF101DataModule(ClassificationDataModule):
         self.eval_dataset = UCF101(
             root=settings.UCF101_PATH,
             annotation_path=self._TRAIN_PATH,
-            frames_per_clip=28,
+            frames_per_clip=24,
             fold=2,
             transform=self.config["test_transform"],
-            step_between_clips=64,
+            step_between_clips=72,
             train=False,
             _precomputed_metadata=val_md,
         )
