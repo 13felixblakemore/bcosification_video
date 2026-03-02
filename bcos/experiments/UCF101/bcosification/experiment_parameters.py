5440de83eb52c0c8b9869a5f46d8fa858d6f8069
13felixblakemore <13felix.blakemore@gmail.com>
2026-03-02 12:18:16 +0000

    workers

--- bcos/experiments/UCF101/bcosification/experiment_parameters.py
+++ bcos/experiments/UCF101/bcosification/experiment_parameters.py
@@ -26,7 +26,7 @@ NUM_CLASSES = 101
 NUM_TRAIN_EXAMPLES: int = 50000 # not sure
 NUM_EVAL_EXAMPLES: int = 3925 # not sure
 
-DEFAULT_BATCH_SIZE = 2  # could be causing noisy gradients?
+DEFAULT_BATCH_SIZE = 4  # could be causing noisy gradients?
 DEFAULT_NUM_EPOCHS = 30
 DEFAULT_LR =1e-4
 DEFAULT_CROP_SIZE = 224
@@ -49,7 +49,7 @@ DEFAULTS = dict(
             is_bcos=True,
         ),
         batch_size=DEFAULT_BATCH_SIZE,
-        num_workers=8,
+        num_workers=4,
         num_classes=NUM_CLASSES,
     ),
     model=dict(
