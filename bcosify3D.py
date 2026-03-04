147437b753bd84ed008ed1ba4503f1acf12b5d4a
13felixblakemore <13felix.blakemore@gmail.com>
2026-03-04 00:38:27 +0000

    wrker

--- bcosify3D.py
+++ bcosify3D.py
@@ -54,9 +54,6 @@ class BcosifyNetwork(BcosUtilMixin, nn.Module):
             for p in self.model.blocks[i].parameters():
                 p.requires_grad = False
 
-        for p in self.model.blocks[0].parameters():
-            p.requires_grad = True
-
         for p in self.model.blocks[6].parameters():
             p.requires_grad = True
 
