313910f064f9984c2e909f4528236d83f3b0cccd
13felixblakemore <13felix.blakemore@gmail.com>
2026-03-12 13:03:41 +0000

    standard

--- bcos/experiments/UCF101/standard/model.py
+++ bcos/experiments/UCF101/standard/model.py
@@ -40,7 +40,7 @@ def get_torch_model_modified(arch_name: str, model_config):
         return model
 
 def get_model(model_config) -> nn.Module:
-    assert model_config.get("is_bcos", False), "Should be False!"
+    assert model_config.get("is_bcos", True), "Should be False!"
     # extract args
     arch_name = model_config["name"]
 
