c392693cc04fe032e566190a807dc2d3b60299e8
13felixblakemore <13felix.blakemore@gmail.com>
2026-03-29 14:18:42 +0100

    bcos bn3d, no bias, b=2

--- bcos/experiments/UCF101/bcosification/experiment_parameters.py
+++ bcos/experiments/UCF101/bcosification/experiment_parameters.py
@@ -27,7 +27,7 @@ NUM_TRAIN_EXAMPLES: int = 50000 # not sure
 NUM_EVAL_EXAMPLES: int = 3925 # not sure
 
 DEFAULT_BATCH_SIZE = 8 # could be causing noisy gradients?
-DEFAULT_NUM_EPOCHS = 18
+DEFAULT_NUM_EPOCHS = 50
 DEFAULT_LR = 1e-4
 DEFAULT_CROP_SIZE = 224
 
