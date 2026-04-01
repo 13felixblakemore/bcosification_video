0085be740e429bef65279b4d636fc94eb1ce0086
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-01 15:46:57 +0100

    cpu

--- bcos/common.py
+++ bcos/common.py
@@ -583,9 +583,9 @@ def gradient_to_video(video, linear_mapping, smooth=15, alpha_percentile=99.5, r
     grad_video = [rgb_grad[:, t].permute(1, 2, 0).detach().cpu().numpy() for t in range(T)]
     #print("Grad video: ", grad_video.shape)
     if return_contribs:
-        return np.array(grad_video), np.array(frame_scores.cpu()), contribs.detach().cpu().numpy()
+        return np.array(grad_video), np.array(frame_scores.detach().cpu()), contribs.detach().cpu().numpy()
     else:
-        return np.array(grad_video), np.array(frame_scores.cpu())
+        return np.array(grad_video), np.array(frame_scores.detach(),cpu())
 
 def gradient_to_image(image, linear_mapping, smooth=15, alpha_percentile=99.5, return_contribs=False):
     """
