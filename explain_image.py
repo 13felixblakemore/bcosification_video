e68e97add17c68ab0f0b55b6a1d765b5a78cb90f
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-01 19:13:09 +0100

    explain

--- explain_image.py
+++ explain_image.py
@@ -128,7 +128,7 @@ def explain_video(args, video_path):
         frames.append(frame)
     cap.release()
 
-    x = 4
+    x = 12
     frames = frames[x:x+8]
 
     transform = UCF101ClassificationPresetEval(
