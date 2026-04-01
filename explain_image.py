7b7e5b1b421f1fd36cbe725ed24cf3cca3c98fba
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-01 17:35:51 +0100

    cpu

--- explain_image.py
+++ explain_image.py
@@ -128,7 +128,7 @@ def explain_video(args, video_path):
         frames.append(frame)
     cap.release()
 
-    frames = frames[:12]
+    frames = frames[60:72]
 
     transform = UCF101ClassificationPresetEval(
         crop_size=224,
