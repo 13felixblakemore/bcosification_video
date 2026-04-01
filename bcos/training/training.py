51b74ec5a01fd242bbbaedcf5d2003fb735ae875
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-01 01:06:47 +0100

    val check interval

--- bcos/training/training.py
+++ bcos/training/training.py
@@ -89,6 +89,7 @@ def run_training(args):
         callbacks=callbacks,
         accumulate_grad_batches=2,
         precision=16,
+        val_check_interval=0.025,
         **trainer_config,
     )
     print("GPUS: ", trainer.num_devices)
