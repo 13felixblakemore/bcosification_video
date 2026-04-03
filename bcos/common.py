4111ecb5544998d7338104754b22f5f5d60d88ca
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-03 17:07:57 +0100

    explain

--- bcos/common.py
+++ bcos/common.py
@@ -580,9 +580,9 @@ def gradient_to_video(video, linear_mapping, smooth=15, alpha_percentile=99.5, r
     # Only show positive contributions
     alpha = torch.where(contribs < 0, 1e-12, alpha)
     if smooth:
-        kT = 1
-        kH = 1
-        kW = 1
+        kT = 8
+        kH = 8
+        kW = 8
         alpha = F.avg_pool3d(
             alpha,
             kernel_size=(kT, kH, kW),
