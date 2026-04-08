8cdb47d84bf8ac9b4671a75258d36dd90a873ae2
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-08 14:34:52 +0100

    debug

--- explain_image.py
+++ explain_image.py
@@ -132,6 +132,7 @@ def debug(model, video_tensor):
     # Fresh leaf tensor with grad enabled
     x = video_tensor.detach().clone().requires_grad_(True)
 
+    print(model.model)
     # Get first block and first conv
     block_0 = model.model.blocks[0]
     block_1 = model.model.blocks[1]
