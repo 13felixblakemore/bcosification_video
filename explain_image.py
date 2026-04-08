df28a1c535ee3957dd02da5eb3417c672a1e5456
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-08 14:28:55 +0100

    debug

--- explain_image.py
+++ explain_image.py
@@ -136,7 +136,7 @@ def debug(model, video_tensor):
     conv = stem.conv
 
     # Forward through only the first conv
-    y = stem.pool(stem.activation(stem.norm(conv(x))))
+    y = stem(x)
     f = y.sum()                  # scalar
 
     # Backward
