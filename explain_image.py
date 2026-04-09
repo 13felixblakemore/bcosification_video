120be9135dc5c9b0ab89777a80daf7bb95a721bb
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-09 12:22:38 +0100

    expla

--- explain_image.py
+++ explain_image.py
@@ -204,7 +204,7 @@ def explain_video(args, video_path):
         is_bcos=True,
     )
     # choose which frame to keep
-    t_keep = [0,1,2,3,4,5,6,7,8,9]  # change this manually each run
+    t_keep = [1,2,3,4,5,6,7,8]  # change this manually each run
 
     # replacement (use mean frame for stability)
     black = np.zeros_like(frames[0])
