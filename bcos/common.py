3523f9f8432ee42c8c9e4d99c26f6ea7073b592c
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-14 14:48:19 +0100

    b

--- bcos/common.py
+++ bcos/common.py
@@ -582,7 +582,7 @@ def gradient_to_video(video, linear_mapping, smooth=1, alpha_percentile=95.0, re
 
     rgb = linear_mapping[:3]
     rgb = rgb / (rgb.abs().max() + 1e-6)
-    rgb = (rgb + 1) / 2
+    #rgb = (rgb + 1) / 2
     for i in range(10):
         print(rgb[:, 0, i, 0])
 
