8d0c423a7f9b2cd8efbaca53e5c44cdc14485818
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-14 14:09:50 +0100

    b

--- bcos/data/presets.py
+++ bcos/data/presets.py
@@ -468,6 +468,7 @@ class UCF101ClassificationPresetEval:
             print("Video shape: ", video.shape)
             print(video[0].max())
             print(video[0].mean())
+            print(video[0,:,0,0])
             sys.exit()
         else:
             mean = torch.tensor(self.mean, device=video.device).view(1, -1, 1, 1)
