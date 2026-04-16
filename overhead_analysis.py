fd6cc8cee4f8fc0aea65e0070e4564f598d428a2
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-16 11:57:40 +0100

    overhead

--- overhead_analysis.py
+++ overhead_analysis.py
@@ -21,6 +21,8 @@ model_bcos = BcosifyNetwork(model, config["model"])
 
 # If checkpoint contains only state_dict
 for model, checkpoint in [(model, s_checkpoint), (model_bcos, checkpoint)]:
+    print(model.state_dict().keys())
+    continue
     if "state_dict" in checkpoint:
         model.load_state_dict(checkpoint["state_dict"])
     else:
