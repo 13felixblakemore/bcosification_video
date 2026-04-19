291fd8212031a9a91eaca54228d815a86b4c3650
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-19 17:10:41 +0100

    exp2

--- bcosify3D.py
+++ bcosify3D.py
@@ -5,6 +5,7 @@ import warnings
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
+from torch.nn import BatchNorm3d
 from torch.utils.checkpoint import checkpoint
 
 from CLIP.clip.model import AttentionPool2d
@@ -99,7 +100,7 @@ class BcosifyNetwork(BcosUtilMixin, nn.Module):
         bcosify_args = model_config.get("bcosify_args", None)
         clip_kd = bcosify_args.get("clip_kd", False) if bcosify_args is not None else False
         for n, module in model.named_children():
-            if isinstance(module, (BcosifyConv3d, BcosifyLinear, BcosSequential, BatchNormUncentered3d)):
+            if isinstance(module, (BcosifyConv3d, BcosifyLinear, BcosSequential, BatchNormUncentered3d, nn.BatchNorm3d)):
                 continue
             if len(list(module.children())) > 0:
                 # compound module, go inside it
