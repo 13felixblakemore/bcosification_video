bb456a0efb8e6b1b7c4495e85755e8e7ec7fc067
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-20 10:57:04 +0100

    pg

--- gp_game2.py
+++ gp_game2.py
@@ -107,7 +107,7 @@ def explain(model, args, video_tensor, labels):
 
         with torch.enable_grad(), model.explanation_mode():
             out = model(x)
-            pred = out.max(1)
+            pred = out.topk(1, 10)
             print(pred.indices.item())
             print(f"quadrant={quadrant}, label={label}, logit={out[0, label].item():.6f}")
 
