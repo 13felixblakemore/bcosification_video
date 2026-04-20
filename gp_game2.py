004dc920708200b942e9a5b3f493776d0f1a45c8
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-20 14:14:56 +0100

    pg

--- gp_game2.py
+++ gp_game2.py
@@ -81,7 +81,7 @@ def game(args):
         model=model,
         loader=loader,
         device=device,
-        confidence_threshold=0.5,  # try 0.5 if this is too strict
+        confidence_threshold=0.2,  # try 0.5 if this is too strict
         max_per_class=4,
         max_batches=100,
     )
