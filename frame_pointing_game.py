09d07c85ab4d57bffc23fda3842354ab84a5ef74
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-20 20:28:02 +0100

    pg

--- frame_pointing_game.py
+++ frame_pointing_game.py
@@ -70,14 +70,14 @@ def game(args):
         device=device,
         confidence_threshold=0.0,  # try 0.5 if this is too strict
         max_per_class=2,
-        max_batches=500,
+        max_batches=20,
     )
 
     print("Found high-confidence clips for", len(clips_by_class), "classes")
 
     total_scores = []
 
-    for step in range(100):
+    for step in range(1):
         print(step)
         clip, label = add_blank_frames(clips_by_class, step)
         #clip, labels = add_second_clip(clips_by_class, step)
