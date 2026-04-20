10c6be3a7c832df0d95a57c03d72d24bf8e26fd1
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-20 23:40:37 +0100

    pg

--- bcos/experiments/UCF101/bcosification/experiment_parameters.py
+++ bcos/experiments/UCF101/bcosification/experiment_parameters.py
@@ -61,7 +61,7 @@ DEFAULTS = dict(
             logit_bias=-math.log(NUM_CLASSES - 1),
         ),
         bcos_args=dict(
-            b=3,
+            b=1.5,
             max_out=1,
         ),
     ),
