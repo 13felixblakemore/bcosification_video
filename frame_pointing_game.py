feecb527581af52e24dadbc0895ae8a2455ad8c4
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-20 20:46:25 +0100

    pg

--- frame_pointing_game.py
+++ frame_pointing_game.py
@@ -68,7 +68,7 @@ def game(args):
         model=model,
         loader=loader,
         device=device,
-        confidence_threshold=0.0,  # try 0.5 if this is too strict
+        confidence_threshold=0.8,  # try 0.5 if this is too strict
         max_per_class=8,
         max_batches=500,
     )
