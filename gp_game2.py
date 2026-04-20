b1205346ced927beeb01c308ca93c2b3112cad54
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-20 12:28:55 +0100

    pg

--- gp_game2.py
+++ gp_game2.py
@@ -313,8 +313,9 @@ def explain(model, args, video_tensor, labels, true_quad=None):
         #    plt.close()
         #sys.exit()
         # B-cos contribution map, not raw grad
+        print("LM: ", linear_mapping.shape)
         linear_mapping = grad.squeeze(0)   # [T, H, W]
-
+        print("LM: ", linear_mapping.shape)
         gp_score = gp_scores_from_linear_map(linear_mapping, quadrant)
         scores.append(gp_score)
     return scores
