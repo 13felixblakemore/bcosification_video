cde75c6cc5e86ebb8e2faa8ad5c3c8ee9d4f3972
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-21 18:45:25 +0100

    pg

--- gp_game2.py
+++ gp_game2.py
@@ -147,7 +147,7 @@ def explain(model, args, video_tensor, labels, true_quad=None):
             pred_class = out.argmax(dim=1).item()
             confidence = F.softmax(out, dim=1)[0, pred_class].item()
 
-            if pred_class == label and confidence > 0.9:
+            if pred_class == label and confidence > 0.3:
                 count += 1
                 pass
             else:
