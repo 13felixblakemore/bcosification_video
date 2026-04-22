e46d416866c9663bd8826f7346ee941ac95ae58a
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-22 20:43:32 +0100

    pg

--- temporal_localisation.py
+++ temporal_localisation.py
@@ -85,7 +85,7 @@ def game(args):
         device=device,
         confidence_threshold=0.0,  # try 0.5 if this is too strict
         max_per_class=20,
-        max_batches=200,
+        max_batches=2,
     )
 
     print("Found high-confidence clips for", len(clips_by_class), "classes")
