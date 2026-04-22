be69e084fc9026f2f9f5b83c141bb91e9dac490a
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-22 10:43:27 +0100

    pg

--- gp_game2.py
+++ gp_game2.py
@@ -266,6 +266,7 @@ def plot_grid(linear_mapping, vid):
     # Only show positive contributions
     alpha = torch.where(contribs < 0, 1e-12, alpha)
     # [1, T, H, W] -> [T, 1, H, W]
+    alpha = alpha.squeeze(0)
     alpha_2d = alpha.permute(1, 0, 2, 3)
     alpha_2d = F.avg_pool2d(alpha_2d, kernel_size=5, stride=1, padding=(5 - 1) // 2)
     alpha = alpha_2d.permute(1, 0, 2, 3)  # back to [1, T, H, W]
