bfc3beba0599f5438a998ee5400736803e16a96b
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-23 15:08:10 +0100

    pg

--- explain_image.py
+++ explain_image.py
@@ -270,7 +270,7 @@ def explain_video(args, video_path=None, vid_tensor=None):
     print("Logit value:", pred_val.item())
     logits = logits[0]
 
-    expl_out = model.explain_video(video_tensor, 41)
+    expl_out = model.explain_video(video_tensor, 66)
     print("Prediction:", idx2label(expl_out["prediction"]))
     print(pred_idx.item(), expl_out["prediction"])
 
