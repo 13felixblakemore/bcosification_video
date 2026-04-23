ac05f7992bea85a31a65bc22c3f561a52d996147
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-23 12:21:53 +0100

    pg

--- temporal_localisation.py
+++ temporal_localisation.py
@@ -231,9 +231,9 @@ def plot_fp_score(vid, linear_mapping, contribs):
     # [1, T, H, W] -> [T, 1, H, W]
     print("Alpha: ", alpha.shape)
     alpha_2d = alpha.permute(1, 0, 2, 3)
-    alpha_2d = F.avg_pool2d(alpha_2d, kernel_size=25, stride=1, padding=(25 - 1) // 2)
+    alpha_2d = F.avg_pool2d(alpha_2d, kernel_size=50, stride=1, padding=(50 - 1) // 2)
     alpha = alpha_2d.permute(1, 0, 2, 3)  # back to [1, T, H, W]
-    alpha = (alpha / torch.quantile(alpha, q=10.0 / 100)).clip(0, 1)
+    alpha = (alpha / torch.quantile(alpha, q=90.0 / 100)).clip(0, 1)
 
     rgb_grad = torch.concatenate([rgb_grad, alpha], dim=0)  # [4, T, H, W]
     T = rgb_grad.shape[1]
