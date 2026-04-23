32cc990bca0b92095ef86dcfa644bc10f5cd25c1
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-23 11:33:02 +0100

    pg

--- grid.py
+++ grid.py
@@ -281,7 +281,7 @@ def add_second_clip(clips_by_class, seed=42):
 
     black_frame = torch.zeros_like(clip[:,0]).unsqueeze(1)  # (C, H, W)
 
-    add_blank = True
+    add_blank = False
     if add_blank:
         clip2 = torch.cat([black_frame, clip2], dim=1)
 
