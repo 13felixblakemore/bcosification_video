2afb489c5a82a544428b7b66e1dca90f9b17fad7
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-23 21:07:48 +0100

    pg

--- faithfulness.py
+++ faithfulness.py
@@ -28,6 +28,8 @@ def get_parser(add_help=True):
 def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model, model_config = load_model_and_config(args)
+
+    args.checkpoint = None
     if args.checkpoint is not None:
         print(f"Loading checkpoint from: {args.checkpoint}")
 
