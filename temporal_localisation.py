ecf9be0b12d98a746df1e04f852ab32c0099041b
13felixblakemore <13felix.blakemore@gmail.com>
2026-04-23 10:32:27 +0100

    pg

--- temporal_localisation.py
+++ temporal_localisation.py
@@ -83,7 +83,7 @@ def game(args):
         model=model,
         loader=loader,
         device=device,
-        confidence_threshold=0.0,  # try 0.5 if this is too strict
+        confidence_threshold=0.5,  # try 0.5 if this is too strict
         max_per_class=30,
         max_batches=100,
     )
