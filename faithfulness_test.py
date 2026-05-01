a198c97e60c53f7f3c3d369b1dba20a3ee8c70e8
13felixblakemore <13felix.blakemore@gmail.com>
2026-05-01 10:21:48 +0100

    faitfhulness

--- faithfulness_test.py
+++ faithfulness_test.py
@@ -69,7 +69,7 @@ def test_faithfulness(args):
             target_classes
         ]
 
-        masked_videos = mask_topk_contributions(model, videos, target_classes, device)
+        masked_videos = mask_topk_contributions(model, videos, target_classes, device, k_percentile=0.2)
         with torch.no_grad():
             masked_logits = model(masked_videos)
             masked_probs = F.softmax(masked_logits, dim=1)
